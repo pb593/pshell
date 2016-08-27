1261b92a9b4deaabe573b0a386748b393e953a77
Pavel Berkovich <pberkovich1994@gmail.com>
2016-08-27 16:40:15 -0400

    Basic input of a line done

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
 #include<iostream> // import new 
+#include<string>
 
 using namespace std;
 
 int main() {
 
-	cout << "Hello!" << endl;
+	// main loop
+	while(true) {
+		
+		string s; // get input line from STDIN 
+		getline(cin, s);
+		cout << "Got line: " << s << endl;
+
+	}
 
 	return 0;
 
