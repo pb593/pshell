3b397559ba251020482dda11e297cb3c254626b1
Pavel Berkovich <pberkovich1994@gmail.com>
2016-08-27 17:45:40 -0400

    This is forking awesome

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,24 @@
-#include<iostream> // import new 
+#include<iostream>
 #include<string>
 #include<sstream>
 #include<algorithm>
 #include<iterator>
 #include<vector>
 
+#include<sys/wait.h>
+#include<unistd.h>
+
 using namespace std;
 
 int main() {
 
+	string PATH("/bin:/usr/bin"); 
+		
 	// main loop
 	while(true) {
 		
+		cout << "> "; // prompt
+			
 		// Command input
 		string input;  
 		getline(cin, input); // get input line from STDIN
@@ -23,10 +30,27 @@ int main() {
 			 istream_iterator<string>(),
 			 back_inserter(tokens)); // tokenize
 
-		// Test tokenization
-		for(auto t: tokens)
-			cout  << t << endl;
+		// Expansion
+		// TODO: aliases, wildcards
 		
+
+		// Fork a new process and wait on it
+		pid_t parent = getpid();
+		cout << "Parent: pid=" << parent << ". Ready to fork!" << endl;
+		pid_t pid = fork();
+		if(pid < 0) { // failed to fork
+			cerr << "Failed to fork!" << endl;
+		}
+		else if(pid > 0) { // parent
+			int status;
+			cout << "Parent: forked OK. Waiting for child." << endl;
+			waitpid(pid, &status, 0);
+			cout << "Parent: Child done. Status = " << status << ". Continuing." << endl;
+		}
+		else { // child
+			cout << "Child: Hello! I am here. pid=" << getpid() << endl;
+			exit(500);
+		}
 
 	}
 
